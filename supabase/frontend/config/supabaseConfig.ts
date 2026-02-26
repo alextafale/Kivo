d69238ed4bf50b9ebcb9bb00af97a1a17fc0e07b
alextafale <tafoyaalex32@gmail.com>
2026-02-26 15:34:25 -0600

    Keys upload

--- supabase/frontend/config/supabaseConfig.ts
+++ supabase/frontend/config/supabaseConfig.ts
@@ -1,5 +1,8 @@
-// frontend/config/supabaseConfig.ts
+
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+
