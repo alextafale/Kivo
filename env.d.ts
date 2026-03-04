cbe01bda6e28f7ba97e6d8cc7bd3295f579b9b59
alextafale <tafoyaalex32@gmail.com>
2026-03-03 20:15:17 -0600

    Keys changed

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,9 @@ declare module '@env' {
   export const SUPABASE_URL: string;
   export const SUPABASE_ANON_KEY: string;
   export const OLLAMA_HOST: string;
+  export const POSTGRES_USER: string;
+  export const POSTGRES_PASSWORD: string;
+  export const POSTGRES_HOST: string;
+  export const POSTGRES_PORT: string;
+  export const POSTGRES_DB: string;
 }
